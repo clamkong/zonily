fc084658faee4bb7984cfd5adfdad6845fa07d11
Chris Lam <christopher.kong.lam@gmail.com>
2018-07-20 14:27:02 -0700

    updated playbook details page with some example content and added author component to it. No styling done

--- src/components/AuthorDetails.js
+++ src/components/AuthorDetails.js
@@ -4,7 +4,7 @@ const AuthorDetails = ({imgUrl, name, blurb}) => (
     <div className="author-details"> 
         <h1>About the Author</h1>
         <div className="author-image"> </div>
-            <img src={imgUrl} alt="author-image"/>
+            <img src={imgUrl} alt="author-image-section"/>
             <p>John Smith's rating</p>
             <p>John Smith's reviews</p>
         <div className="author-info"> 
--- src/components/PlaybookDetailsPage.js
+++ src/components/PlaybookDetailsPage.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import AuthorDetails from './AuthorDetails';
 
 export default class PlaybookDetailsPage extends React.Component {
     render() {
         return (
-            `this is the playbook details page for page this for ${this.props.match.params.pId}`
+            <div className="playbook-details">
+                <div className="playbook-details-content"> 
+                    <div className="playbook-title-info">
+                        <h1>Playbook Title</h1>
+                        <h3>Playbook gist</h3>
+                        <h3>Playbook rating </h3>
+                        <h3># of people who bought playbook</h3>
+                        <h2>Created by John Smith</h2>
+                        <h4>Language: English</h4>
+                    </div>
+                    <div className="video-section">Playbook intro video</div>
+                    <div className="what-you-learn">
+                        <h2>What will I learn?</h2>
+                        <p>Blah blah blah</p>
+                    </div>
+                    <div className="playbook-outline">
+                        <h2>Playbook Outline</h2>
+                        <ul>
+                            <li>Chapter 1</li>
+                            <li>Chapter 2</li>
+                            <li>Chapter 3</li>
+                            <li>Chapter 4</li>
+                        </ul>
+                    </div>
+                    <AuthorDetails />
+                    {`this is the playbook details page for page this for ${this.props.match.params.pId}`}
+                </div>
+                div for shopping widget here
+                
+            </div>
         );
     }
 }

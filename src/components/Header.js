aadd187dce7e2c0273ccf47c8925e81a14eb2cc1
Chris Lam <christopher.kong.lam@gmail.com>
2018-07-20 16:20:46 -0700

    added some styling and layout organization to nav links and header buttons. Changed color from that orange thing to white

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,58 @@
 import React from 'react';
 import logoImg from '../images/Zonily Logo.png';
-import { NavLink } from 'react-router-dom';
+import { NavLink, withRouter } from 'react-router-dom';
+import {Button, Icon} from 'semantic-ui-react';
 
-const Header = () => (
+const Header = withRouter(({history}) => (
     <header className="app-header">
-        <NavLink to="/" activeClassName="is-active" exact={true}>
-            <img className="logo" src={logoImg} alt="logo" />
-            <div className="company-name"><h1>Zonily</h1></div>
-        </NavLink>
-        <NavLink to="/marketplace" activeClassName="is-active">Marketplace</NavLink>
-        <NavLink to="/about" activeClassName="is-active">About</NavLink>
-        <NavLink to="/signup" activeClassName="is-active">Signup</NavLink>
-        <NavLink to="/login" activeClassName="is-active">Login</NavLink>
+        <div className="app-header__inner">
+            <div className="header__inner__left">
+                <div className="logo-container">
+                    <NavLink to="/" activeClassName="is-active" exact={true}>
+                        <img className="logo" src={logoImg} alt="logo" />
+                    </NavLink>
+                </div>
+            </div>
+            <div className="header__inner__right">
+                <div className="header-nav">
+                    <span className="header--nav-item">
+                        <NavLink to="/marketplace" activeClassName="is-active">Marketplace</NavLink>
+                    </span>
+                    <span className="header--nav-item">
+                        <NavLink to="/about" activeClassName="is-active">About</NavLink>
+                    </span>
+                </div>
+                <div className="shopping-cart">
+                    <Button 
+                        className="header__cart-item"
+                        icon
+                    >
+                        <Icon name="cart" />
+                    </Button>
+                </div>
+                <div className="header--login-signup">
+                    <Button
+                        className="header__login-item"
+                        primary
+                        onClick={() => {
+                            history.push('/login');
+                        }}
+                    >
+                        Log In
+                    </Button>
+                    <Button
+                        className="header__signup-item"
+                        secondary
+                        onClick={() => {
+                            history.push('/signup');
+                        }}
+                    >
+                        Sign Up
+                    </Button>
+                </div>
+            </div>
+        </div>
     </header>
-)
+))
 
 export default Header;

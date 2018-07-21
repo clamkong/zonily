29c1198811640b41d7ded144daf94c78f03c0b51
Christopher Lam <christopher.kong.lam@gmail.com>
2018-07-20 23:32:04 -0700

    Refactor some styling for displaying playbooks
    there was some unneeded css that was causing the group to mis-align the cards. Also now we will hard code in the card sizes so it looks better till we can get some real content inside.

--- src/components/PlaybookCard.js
+++ src/components/PlaybookCard.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Card } from 'semantic-ui-react'
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-const PlaybookCard = ({name, id}) => (
+const PlaybookCard = ({ name, id }) => (
     <Card
         href={`/playbook/${id}`}
+        className="playbook-card"
     >
         <Card.Content>
             <Card.Header>{name}</Card.Header>
---
+++ src/data/mock/myPlaybooks.js
@@ -0,0 +1,16 @@
+const myPlaybooks = [
+    {
+        id: 0,
+        name: "Playbook0"
+    },
+    {
+        id: 1,
+        name: "Playbook1"
+    },
+    {
+        id: 2,
+        name: "Playbook2"
+    }
+];
+
+export default myPlaybooks;

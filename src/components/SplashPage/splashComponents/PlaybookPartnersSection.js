5b6cdf2a4de21b1ce8c4a328efbdff36523ed3fd
Mack Siu <msiu23@gmail.com>
2018-09-15 14:57:50 -0700

    fix react router issues

---
+++ src/components/SplashPage/splashComponents/PlaybookPartnersSection.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import avatar1 from '../../../images/avatars/avatar-1.jpg'
+import avatar2 from '../../../images/avatars/avatar-2.jpg'
+
+const PlaybookPartnersSection = () => {
+    return (
+        <section id="testimonials">
+            <div className="row section-intro">
+                <div className="col-twelve with-bottom-line">
+                    <h5>Partners</h5>
+                    <h1>Who You'll Learn From</h1>
+                </div>
+            </div>
+            <div className="row flex-container">
+                Our current partners have businesses earning between $1 million - $25 million in revenue.
+                <div>We are currently developing their playbooks! Stay tuned!</div>
+            </div>
+        </section>
+    )
+}
+
+export default PlaybookPartnersSection;

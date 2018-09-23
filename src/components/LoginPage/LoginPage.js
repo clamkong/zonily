422972817784ea28cce867189625a34097d80782
Christopher Lam <christopher.kong.lam@gmail.com>
2018-09-23 14:25:30 -0700

    Login Page UI
    Routing included from header page. Non functioning controls. Need Forget Password page

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -1,9 +1,56 @@
-import React from 'react';
+import React from "react";
+import "../../styles/components/LoginPage.css";
 
 export default class LoginPage extends React.Component {
-    render() {
-        return (
-            "this is the login page"
-        );
-    }
+  render() {
+    return (
+      <div className="login-page">
+        <div className="login-page-tile">
+          <div className="container">
+            <div className="login-page-title">
+              <h1>LOG IN</h1>
+            </div>
+            <div className="login-page-form">
+              <input
+                id="login-page-form__email"
+                name="email"
+                placeholder="Email"
+                required="required"
+                type="email"
+              />
+              <input
+                id="login-page-form__password"
+                name="password"
+                ng-model="password"
+                placeholder="Password"
+                type="password"
+              />
+              <p id="login-page-form__forgot-password">
+                <a className="forgot mc-modal-reset-trigger" href="#">
+                  {" "}
+                  Forgot your password?
+                </a>
+              </p>
+              <button
+                className="login-page-form__submit"
+                id="login-page-form__submit-button"
+                type="submit"
+              >
+                {" "}
+                Log in
+              </button>
+              <p className="button_desc">
+                Need an account?{" "}
+                <a className="login-page-form__register-trigger" href="#">
+                  {" "}
+                  Create an account.
+                </a>
+              </p>
+            </div>
+            <div className="login-page-form__footer" />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
--- src/components/SplashPage/splashComponents/Header.js
+++ src/components/SplashPage/splashComponents/Header.js
@@ -1,43 +1,63 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default class Header extends React.Component {
   render() {
-  return (
+    return (
       <header>
-          <div className="row">
-              <div className="logo">
-                  <a href="/">Zonily</a>
-              </div>
-              { this.props.preorder ? <div/> : (
-              <nav id="main-nav-wrap">
-                  <ul className="main-navigation">
-                      <li className="current">
-                          <a className="smoothscroll" href="#intro" title="">Home</a>
-                      </li>
-                      <li>
-                          <a className="smoothscroll" href="#process" title="">About</a>
-                      </li>
-                      <li>
-                          <a className="smoothscroll" href="#features" title="">Playbooks</a>
-                      </li>
-                      <li>
-                          <a className="smoothscroll" href="#testimonials" title="">Partners</a>
-                      </li>
-                      <li>
-                          <a className="smoothscroll" href="#pricing" title="">Pricing</a>
-                      </li>
-                      <li>
-                          <a className="smoothscroll" href="#contact-us" title="">Contact Us</a>
-                      </li>
-
-                  </ul>
-              </nav>
-            )}
-            <a className="menu-toggle" href="#">
-                <span>Menu</span>
-            </a>
+        <div className="row">
+          <div className="logo">
+            <a href="/">Zonily</a>
           </div>
+          {this.props.preorder ? (
+            <div />
+          ) : (
+            <nav id="main-nav-wrap">
+              <ul className="main-navigation">
+                <li className="current">
+                  <a className="smoothscroll" href="#intro" title="">
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a className="smoothscroll" href="#process" title="">
+                    About
+                  </a>
+                </li>
+                <li>
+                  <a className="smoothscroll" href="#features" title="">
+                    Playbooks
+                  </a>
+                </li>
+                <li>
+                  <a className="smoothscroll" href="#testimonials" title="">
+                    Partners
+                  </a>
+                </li>
+                <li>
+                  <a className="smoothscroll" href="#pricing" title="">
+                    Pricing
+                  </a>
+                </li>
+                <li>
+                  <a className="smoothscroll" href="#contact-us" title="">
+                    Contact Us
+                  </a>
+                </li>
+                <li class="highlight with-sep">
+                  <Link to="/signup">Sign Up</Link>
+                </li>
+                <li class="highlight">
+                  <Link to="/login">Login</Link>
+                </li>
+              </ul>
+            </nav>
+          )}
+          <a className="menu-toggle" href="#">
+            <span>Menu</span>
+          </a>
+        </div>
       </header>
-    )
+    );
   }
 }

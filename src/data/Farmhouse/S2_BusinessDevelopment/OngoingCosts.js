f26f8b34dfa9d32a5c40c62733c9759301d65c71
Mack Siu <msiu23@gmail.com>
2018-10-22 00:00:43 -0700

    data up to chapter 2

--- src/data/Farmhouse/S2_BusinessDevelopment/OngoingCosts.js
+++ src/data/Farmhouse/S2_BusinessDevelopment/OngoingCosts.js
@@ -1,13 +1,39 @@
 export default data = {
-  type: PIE,
+  type: "PIE",
   title: "Ongoing Costs",
   chapterId: 2,
   data: [
     {
-
+      title: 'Pay yourself as a high pay employee rather than taking out directly from the corporation (payroll is deducted from the company)'
+    },
+    {
+      title: 'Income tax, employee tax, simple tax (air pollution tax, soda tax, recycling tax, etc.), sales tax, etc.'
+    },
+    {
+      title: 'Pay your taxes weekly so the IRS won’t question you differences between years (especially first year to second year)'
+    },
+    {
+      title: 'Don’t pay yourself too high, save money in the corporation so you can use the money to buy another one (otherwise you will pay double taxes, income and sales tax)'
+    },
+    {
+      title: 'Don’t pay yourself too high, save money in the corporation so you can use the money to buy another one (otherwise you will pay double taxes, income and sales tax)',
+      data: [
+        'Labor – 55%',
+        'Food – 24%',
+        'Rent - <6%',
+        'Marketing - 2-3% ($3-5,000 per month)',
+        'Miscellaneous (equipment, cleaning)',
+        'Profit – 8-10% ($14-16,000 per month)'
+      ]
+    },
+    {
+      title: '$4 million in the first year, 20-30% growth year after year ($6-7 million in 4-5 years',
+      data: [
+        'Know what to expect each season and month in terms of revenue',
+        'Knowing what to expect so that you can lock in food costs with guarantee when the price is lower for the higher times'
+      ]
     }
-        
-    ]
+  ]
 }
 
 const data2 = [

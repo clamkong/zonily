62c4514fbdea792b0347fd98091eae5638620aaa
Mack Siu <msiu23@gmail.com>
2018-10-23 23:38:40 -0700

    chapter 2 updated model

--- src/data/Farmhouse/S2_BusinessDevelopment/C5_RestaurantConcept.js
+++ src/data/Farmhouse/S2_BusinessDevelopment/C5_RestaurantConcept.js
@@ -6,7 +6,33 @@ const data = {
       subChapterId: 0,
       title: "Farmhouse Thai Kitchen Concept",
       type: "STACK",
-      data: []
+      data: [
+        {
+          title: "Focus on the right customer, age and demographic",
+          content: [
+            "25-60 years old",
+            "Middle Income - Not high income because otherwise want too many things (they won’t like it because too loud)"
+          ]
+        },
+        {
+          title: "Have unique features of your restaurant",
+          content: [
+            "Birthday song (exciting, fun, energetic)",
+            "Grandma’s hot sauce",
+            "Video (fun, makes you laugh)",
+            "Special items (chicken and beef short rib)",
+            "Unique and fun cocktails"
+          ]
+        },
+        {
+          title: "Overall business development summary",
+          content: [
+            "Yourself – know what you want and what to expect",
+            "Your team – who can you trust?",
+            "Understand the legal and tax system"
+          ]
+        }
+      ]
     }
   ]
 };
